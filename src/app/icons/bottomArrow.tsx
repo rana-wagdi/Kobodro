ab059ef6c32796db2b7fcde9f1be89a04fa7bfda
Rana Wagdi <73128713+rana-wagdi@users.noreply.github.com>
2024-11-18 02:39:18 +0200

    feat: add icons

---
+++ src/app/icons/bottomArrow.tsx
@@ -0,0 +1,20 @@
+export default function BottomArrow(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+    {...props}
+      width="13"
+      height="8"
+      viewBox="0 0 13 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.55859 1.5L6.55859 6.5L11.5586 1.5"
+        stroke="#667085"
+        stroke-width="1.67"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/app/icons/index.tsx
@@ -0,0 +1,7 @@
+import Instagram from "./instagram"
+import Facebook from "./Facebook";
+import Menu from "./menu";
+import Twitter from "./Twitter";
+import AppStore from "./appStore";
+
+export { Instagram, Facebook, Twitter, Menu, AppStore};
